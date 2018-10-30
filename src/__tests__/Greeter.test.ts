022e3a1f74536cdd064e4ee7e920fa3e7268d06a
lil-works <lilian@lil-works.com>
2018-10-30 20:50:11 +0100

    1.1.0

--- src/__tests__/Greeter.test.ts
+++ src/__tests__/Greeter.test.ts
@@ -10,12 +10,9 @@ var xy = {
   octave: 3,
   openstring: null,
   selected: true,
-  tone: {id: 7, digit: 6, playerName: "F#", defaultColor: "#0FAFC0", color: "#0FAFC0"},
+  tone: { id: 7, digit: 6, playerName: 'F#', defaultColor: '#0FAFC0', color: '#0FAFC0' },
   x: 4,
-  y: 4
-}
+  y: 4,
+};
 
-var input = [
-  {x:1,y:3},
-  {x:2,y:5}
-];
+var input = [{ x: 1, y: 3 }, { x: 2, y: 5 }];
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,20 @@
 export const Greeter = (name: string) => `Hello ${name}`;
 
-
 export const JnSelectionElementToVextab = (xy: any) => {
-  return {x: xy.x,y:xy.y}
-}
+  return { x: xy.x, y: xy.y };
+};
 
 export const JnSelectionToVextab = (xys: any) => {
   const s = [];
   xys.forEach(xy => {
-      s.push(JnSelectionElementToVextab(xy))
-   });
-  return s
-}
+    s.push(JnSelectionElementToVextab(xy));
+  });
+  return s;
+};
 export const VextabSelectionToString = (vxs: any) => {
   const a = [];
   vxs.forEach(vx => {
-      a.push(vx.x+"/"+vx.y)
-   });
-  return a.join(" ");
-}
+    a.push(vx.x + '/' + vx.y);
+  });
+  return a.join(' ');
+};
